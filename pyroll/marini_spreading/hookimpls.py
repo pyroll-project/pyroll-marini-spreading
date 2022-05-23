0d434015361bf7d1987867c3475cf0740a6c4013
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2022-05-23 12:44:03 +0200

    changed nominal to working radius

--- pyroll/marini_spreading/hookimpls.py
+++ pyroll/marini_spreading/hookimpls.py
@@ -16,12 +16,12 @@ def equivalent_height_change(roll_pass: RollPass):
 
 @RollPass.hookimpl
 def marini_parameter_a(roll_pass: RollPass):
-    return np.sqrt(roll_pass.equivalent_height_change) / (2 * roll_pass.friction_coefficient * np.sqrt(roll_pass.roll.nominal_radius))
+    return np.sqrt(roll_pass.equivalent_height_change) / (2 * roll_pass.friction_coefficient * np.sqrt(roll_pass.roll.working_radius))
 
 
 @RollPass.hookimpl
 def marini_parameter_b(roll_pass: RollPass):
-    return np.sqrt(roll_pass.equivalent_height_change / roll_pass.roll.nominal_radius)
+    return np.sqrt(roll_pass.equivalent_height_change / roll_pass.roll.working_radius)
 
 
 @RollPass.hookimpl
@@ -29,7 +29,7 @@ def spread(roll_pass: RollPass):
     log = logging.getLogger(__name__)
 
     numerator = 2 * roll_pass.equivalent_height_change * roll_pass.in_profile.equivalent_rectangle.width * (
-            roll_pass.roll.nominal_radius - roll_pass.in_profile.equivalent_rectangle.height / 2) * roll_pass.marini_parameter_b
+            roll_pass.roll.working_radius - roll_pass.in_profile.equivalent_rectangle.height / 2) * roll_pass.marini_parameter_b
 
     first_denominator = roll_pass.out_profile.equivalent_rectangle.height * roll_pass.in_profile.equivalent_rectangle.width
 
@@ -40,7 +40,7 @@ def spread(roll_pass: RollPass):
     third_denominator = (0.91 * (roll_pass.in_profile.equivalent_rectangle.width + 3 * roll_pass.in_profile.equivalent_rectangle.height)) / (
             4 * roll_pass.in_profile.equivalent_rectangle.height)
 
-    fourth_denominator = 2 * roll_pass.out_profile.equivalent_rectangle.height * roll_pass.roll.nominal_radius * roll_pass.marini_parameter_b
+    fourth_denominator = 2 * roll_pass.out_profile.equivalent_rectangle.height * roll_pass.roll.working_radius * roll_pass.marini_parameter_b
 
     spread = 1 + (numerator / (first_denominator + second_denominator * third_denominator + fourth_denominator)) / roll_pass.in_profile.equivalent_rectangle.width
 

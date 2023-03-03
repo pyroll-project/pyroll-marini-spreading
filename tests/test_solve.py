2bf48b4ac43da400ecd09fdb4d212ca24ab813c8
Christoph Renzing <64863686+ChRen95@users.noreply.github.com>
2023-03-03 14:17:50 +0100

    Upd 2.0 (#1)
    * removed poetry, added hatch
    
    * collapsed to single py
    
    * added new test
    
    * upd 2.0

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -1,38 +1,75 @@
 import logging
+import webbrowser
 from pathlib import Path
 
-from pyroll.core import solve
-from pyroll.ui import Reporter, Exporter
-from input_imf_continuous_rolling_plant import in_profile, sequence
+from pyroll.core import Profile, PassSequence, RollPass, Roll, CircularOvalGroove, Transport, RoundGroove
 
 
 def test_solve(tmp_path: Path, caplog):
-    caplog.set_level(logging.DEBUG, logger="pyroll")
+    caplog.set_level(logging.INFO, logger="pyroll")
 
-    import pyroll
     import pyroll.marini_spreading
-    import pyroll.lendl_equivalent_method
-    import pyroll.hitchcock_roll_flattening
-    import pyroll.hensel_power_and_labour
-    import pyroll.freiberg_flow_stress
-    import pyroll.integral_thermal
 
-    solve(sequence, in_profile)
+    in_profile = Profile.round(
+        diameter=30e-3,
+        temperature=1200 + 273.15,
+        strain=0,
+        material=["C45", "steel"],
+        flow_stress=100e6,
+        density=7.5e3,
+        thermal_capacity=690,
+    )
 
-    report = Reporter()
-    export = Exporter()
+    sequence = PassSequence([
+        RollPass(
+            label="Oval I",
+            roll=Roll(
+                groove=CircularOvalGroove(
+                    depth=8e-3,
+                    r1=6e-3,
+                    r2=40e-3
+                ),
+                nominal_radius=160e-3,
+                rotational_frequency=1
+            ),
+            gap=2e-3,
+            coulomb_friction_coefficient=0.4,
+        ),
+        Transport(
+            label="I => II",
+            duration=1
+        ),
+        RollPass(
+            label="Round II",
+            roll=Roll(
+                groove=RoundGroove(
+                    r1=1e-3,
+                    r2=12.5e-3,
+                    depth=11.5e-3
+                ),
+                nominal_radius=160e-3,
+                rotational_frequency=1
+            ),
+            gap=2e-3,
+            coulomb_friction_coefficient=0.4,
+        ),
+    ])
 
-    export_file = tmp_path / "export.csv"
-    report_file = tmp_path / "report.html"
+    try:
+        sequence.solve(in_profile)
+    finally:
+        print("\nLog:")
+        print(caplog.text)
 
-    rendered = report.render(sequence)
-    exported = export.export(sequence, "csv")
-    print()
+    try:
+        import pyroll.report
 
-    export_file.write_bytes(exported)
-    report_file.write_text(rendered)
-    print(report_file)
-    print(export_file)
+        report = pyroll.report.report(sequence)
 
-    print("\nLog:")
-    print(caplog.text)
+        report_file = tmp_path / "report.html"
+        report_file.write_text(report)
+        print(report_file)
+        webbrowser.open(report_file.as_uri())
+
+    except ImportError:
+        pass
